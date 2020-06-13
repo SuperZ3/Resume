bdcfc95579356f4dbbf5a34d39e55947b3e1146f
Super ZZZ <zhangzz1024@163.com>
2020-06-13 22:47:21 +0800

    node update

---
+++ Project/React/public/precache-manifest.5f929fb0fca7a35ff3534d4ebb0bd441.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0a7a0a20a1a29760bec6b1eefdc3c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25fb5cf7a1f41f520fe0",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "4ac4128b6edd809c8b13",
+    "url": "/static/css/main.f084c4e6.chunk.css"
+  },
+  {
+    "revision": "25fb5cf7a1f41f520fe0",
+    "url": "/static/js/2.8220dede.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.8220dede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac4128b6edd809c8b13",
+    "url": "/static/js/main.eb16c855.chunk.js"
+  },
+  {
+    "revision": "b800d193b437c35fab0d",
+    "url": "/static/js/runtime-main.30b16838.js"
+  },
+  {
+    "revision": "44f9e3067c8b360837162eff63c61869",
+    "url": "/static/media/login.44f9e306.png"
+  }
+]);
